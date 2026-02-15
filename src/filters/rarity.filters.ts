db9ff8e1d991d8015c043d2aa9ca353d5e6d2f44
ssogorovic <ssogorovic@gmail.com>
2026-02-15 11:07:33 +0100

    fix: replace variable isValid with inline if check

--- src/filters/rarity.filters.ts
+++ src/filters/rarity.filters.ts
@@ -7,17 +7,18 @@ const rarityList = Object.values(rarities);
 export const rarityFilters = {
   all: (): Rarity[] => rarityList,
   byId: (id: number): Rarity | null => {
-    const validId = isValidId(id);
-    if (!validId) return null;
+    if (!isValidId(id)) return null;
     return rarityList.find((r) => r.id === id) ?? null;
   },
   byName: (name: string): Rarity | null => {
     const search = sanitizeSearchString(name);
     if (!search) return null;
-    return rarityList.find((r) => {
-    const normalizedDataName = r.name.trim().toLowerCase();
-    return normalizedDataName === search;
-  }) ?? null;
+    return (
+      rarityList.find((r) => {
+        const normalizedDataName = r.name.trim().toLowerCase();
+        return normalizedDataName === search;
+      }) ?? null
+    );
   },
   byValue: (val: 4 | 5): Rarity[] => rarityList.filter((r) => r.rarity === val),
 };
