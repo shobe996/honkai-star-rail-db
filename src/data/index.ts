e9104d2c6cdfa6b6cbcff250af9fe7ee58187a0d
ssogorovic <ssogorovic@gmail.com>
2026-04-10 21:12:15 +0200

    fix: replace rarities with character and light cone rarities

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,6 @@
+export * from './character-rarities';
 export * from './factions';
 export * from './paths';
-export * from './rarities';
 export * from './types';
 
 // Characters must be last due to circular references
